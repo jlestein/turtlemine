e62d0c30db0c1555fdec50986901f488bd4cd0ac
jstein@mailjar.com <jstein@mailjar.com@1e486504-3536-f062-fe27-aef94824ac5b>
2012-04-11 20:57:20 +0000

    Updating version to 2.0 for Dev and fixing bug in url.

--- trunk/src/TurtleMineShared/version.cs
+++ trunk/src/TurtleMineShared/version.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("http://code.google.com/p/TurtleMine/")]
-[assembly: AssemblyCopyright("Copyright (c) 2011 Joshua Stein")]
+[assembly: AssemblyCompany("http://code.google.com/p/turtlemine/")]
+[assembly: AssemblyCopyright("Copyright (c) 2012 Joshua Stein")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.0.1")]
-[assembly: AssemblyFileVersion("1.2.0.1")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
